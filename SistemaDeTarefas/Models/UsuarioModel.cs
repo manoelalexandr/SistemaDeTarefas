9a58fed330e899ed4a8b688fc510728b0e160f59
Manoel Alexandre <manoelalexandr@gmail.com>
2023-01-09 13:42:23 -0300

    Add project files.

---
+++ SistemaDeTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
